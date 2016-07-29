ed520694b9997690a6893f08b3b6ba210e3492d5
Davide Malvezzi <malvezziai@gmail.com>
2016-07-29 12:41:01 +0200

    Started edit channel modal

--- index.php
+++ index.php
@@ -52,6 +52,7 @@
 
 <body>
 
+  <!--menu and table-->
   <div class="container-fluid">
 
     <div class="row">
@@ -97,12 +98,12 @@
 
   <!--delete modal-->
   <div id="remove-channel-modal" class="modal fade" role="dialog">
-    <div class="modal-dialog modal-sm">
+    <div class="modal-dialog modal-xs">
 
       <div class="modal-content">
 
         <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <button type="button" class="close" data-dixsiss="modal">&times;</button>
           <h4 class="modal-title">Remove</h4>
         </div>
 
@@ -111,8 +112,8 @@
         </div>
 
         <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal" onclick="removeChannel()">Yes</button>
-          <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+          <button type="button" class="btn btn-default" data-dixsiss="modal" onclick="removeChannel()">Yes</button>
+          <button type="button" class="btn btn-default" data-dixsiss="modal">No</button>
         </div>
       </div>
 
@@ -120,36 +121,98 @@
   </div>
 
   <!--edit modal-->
-  <div id="edit-channel-modal" class="modal fade">
-    <div class="modal-dialog">
+  <div class="modal fade" id="edit-channel-modal" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+    <div class="modal-dialog modal-md">
       <div class="modal-content">
         <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
           <h4 class="modal-title">Edit</h4>
         </div>
+        <div class="modal-body form-horizontal">
 
-        <div class="modal-body">
-          <form class="form-horizontal">
             <div class="form-group">
-
-              <label for="channel-id" class="control-label col-sm-1">ID (hex):</label>
-              <div class="col-sm-11">
+              <label for="channel-id" class="control-label col-xs-2">ID (hex):</label>
+              <div class="col-xs-10">
                 <input type="text" id="channel-id" class="form-control" maxlength="4">
               </div>
-
-              <label for="channel-name" class="control-label col-sm-1">Name:</label>
-              <div class="col-sm-11">
-                <input type="text" id="channel-name" class="form-control">
-              </div>
-
             </div>
-          </form>
-        </div>
 
+            <div class="form-group">
+              <label for="channel-name" class="control-label col-xs-2">Name:</label>
+              <div class="col-xs-10">
+                <input type="text" id="channel-name" class="form-control" maxlength="8">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="channel-type" class="control-label col-xs-2">Type:</label>
+              <div class="col-xs-10">
+                <select id="channel-type" class="form-control">
+                  <?php
+                    $sql = "SELECT * FROM data_type ORDER BY combo_order";
+                    $result = $conn->query($sql);
+                    while($row = $result->fetch_assoc()){
+                      echo '<option value="' . $row["id"] . '">' . $row["name"] . '</option>';
+                    }
+                  ?>
+                </select>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="channel-size" class="control-label col-xs-2">Size:</label>
+              <div class="col-xs-10">
+                <select id="channel-size" class="form-control">
+                  <?php
+                    for($i = 1; $i <= 8; $i++){
+                      echo '<option value="' . $i . '">' . $i . '</option>';
+                    }
+                  ?>
+                </select>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="channel-def-value" class="control-label col-xs-2">Default:</label>
+              <div class="col-xs-10">
+                <input type="text" class="form-control" id="channel-def-value">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="channel-min-value" class="control-label col-xs-2">Min:</label>
+              <div class="col-xs-10">
+                <input type="text" class="form-control" id="channel-min-value">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="channel-max-value" class="control-label col-xs-2">Max:</label>
+              <div class="col-xs-10">
+                <input type="text" class="form-control" id="channel-max-value">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="channel-formula" class="control-label col-xs-2">Formula:</label>
+              <div class="col-xs-10">
+                <input type="text" class="form-control" id="channel-formula">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="channel-description" class="control-label col-xs-2">Max:</label>
+              <div class="col-xs-10">
+                <input type="text" class="form-control" id="channel-description">
+              </div>
+            </div>
+
+
+        </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal" onclick="addNewChannel()">OK</button>
+          <button type="button" class="btn btn-default" data-dismiss="modal" onClick="">Save</button>
           <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
         </div>
-
       </div>
     </div>
   </div>
