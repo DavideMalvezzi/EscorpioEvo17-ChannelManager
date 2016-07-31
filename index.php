4d1c3e5c4e2c6ad388309827f4ccd1eef888f17a
Davide Malvezzi <malvezziai@gmail.com>
2016-07-31 11:14:06 +0200

    Changed favicon

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link href="img/favicon.ico" rel="icon" type="image/vnd.microsoft.icon">
+  <link href="img/favicon.png?v=2" rel="icon">
 
   <!--bootstrap css-->
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
